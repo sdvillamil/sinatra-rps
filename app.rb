cddab9545469ba617168460e0b02702044eeb3bc
sdvillamil <156153167+sdvillamil@users.noreply.github.com>
2024-10-22 12:40:39 +0000

    sd

--- app.rb
+++ app.rb
@@ -7,3 +7,33 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+
+  moves = ["rock","paper","scissors"]
+
+  comp_move = moves.sample
+
+  if comp_move == "rock"
+    outcome = "We Tied!"
+  end
+  if comp_move == "scissors"
+    outcome = "You Won!"
+  end
+  if comp_move == "paper"
+    outcome = "You Lost!"
+  end
+  "
+  <h2>
+    We played rock!
+  </h2>
+
+  <h2>
+   They played #{comp_move}!
+  </h2>
+
+  <h2>
+    #{outcome}!
+  </h2>
+  "
+end
